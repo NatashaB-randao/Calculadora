0ec03eb681dcaee1a6da26ef1464daff42232b08
Natasha Brandão <79339286+NatashaB-randao@users.noreply.github.com>
2023-02-15 14:42:40 -0300

    MainActivity

--- app/src/main/java/com/calculadora/calculadora/MainActivity.kt
+++ app/src/main/java/com/calculadora/calculadora/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.calculadora.calculadora
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
     }
+
+    fun AcrescentarUmaExpressao(string: String, limpar_dados : Boolean){
+
+        if (R.id.resultado.text.isNotEmpty){
+            R.id.expressao.text = ""
+        }
+
+        if (limpar_dados){
+            R.id.resultado.text = ""
+            R.id.expressao.append(string)
+
+        }else{
+            R.id.expressao.append(R.id.resultado.text)
+        }
+    }
+
+
+
 }
